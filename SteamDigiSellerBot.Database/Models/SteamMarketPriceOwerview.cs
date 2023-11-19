c620151efced25f5b563a8bf7fb0bf18aa8f4ecb
Dmitry Danilin <dmitrydnl99@gmail.com>
2023-11-19 14:06:36 +0300

    restore

--- SteamDigiSellerBot.Database/Models/SteamMarketPriceOwerview.cs
+++ SteamDigiSellerBot.Database/Models/SteamMarketPriceOwerview.cs
@@ -72,7 +72,7 @@ namespace SteamDigiSellerBot.Database.Models
                 Debug.WriteLine($"{str} ---> {number}");
                 return true;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 Debug.WriteLine($"err parse - {str} -> {newStr}");
 
