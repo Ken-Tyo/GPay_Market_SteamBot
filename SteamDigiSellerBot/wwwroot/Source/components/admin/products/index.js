f0ae0322a820df5b3fd88a474454f9c38ded947e
Рогов Артем Алексеевич <artem.rogov@trulysocialgames.com>
2024-05-08 01:09:10 +0300

    worked with logs

--- SteamDigiSellerBot/wwwroot/Source/components/admin/products/index.js
+++ SteamDigiSellerBot/wwwroot/Source/components/admin/products/index.js
@@ -1,52 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import List from './list';
 import PriceHierarchy from './priceHierarchy';
 import PageHeader from '../pageHeader';
 import css from './styles.scss';
 import { state } from '../../../containers/admin/state';
 import { itemsMode as mode } from '../../../containers/admin/common';
+import { ScrollRestoration , Link } from "react-router-dom";
 
 const products = () => {
   const { itemsMode } = state.use();
-  const listRef = React.createRef();
 
-  var prevItemMode = itemsMode;
+  const contentRef = useRef(null);
+
   useEffect(() =>{
-    console.log("wrapper lists " + listRef?.current?.scrollTop);
-    listRef?.current?.addEventListener("scroll", event => {
-      console.log(`scrollTop: ${listRef?.current?.scrollTop} <br>
-                          scrollLeft: ${listRef?.current?.scrollLeft} `);
-  }, { passive: true });
-    if(prevItemMode === mode[2] & itemsMode === mode[1] & localStorage.getItem("scrollTop")){
+    var ara = contentRef.current.scrollTop;
+    console.log("IN itemsmode " + itemsMode + " scrollTop " + localStorage.getItem("scrollTop") + " " + (contentRef.current != null))
+    if(itemsMode === mode[1] & localStorage.getItem("scrollTop") != null){
       // скроллим к сохраненным координатам
-      listRef.current.scroll({
-        top: localStorage.getItem("scrollTop"),
-        behavior: "smooth"
+      console.log("scrollTo " + localStorage.getItem("scrollTop"));
+      contentRef.current.scroll({
+        top: localStorage.getItem("scrollTop")
       });
       // удаляем данные с localStorage
       localStorage.removeItem("scrollTop");
     }
-    prevItemMode = itemsMode;
+
     return () => {
-      if(prevItemMode === mode[1] & itemsMode === mode[2]){
-        localStorage.setItem("scrollTop", listRef.current.scrollTop);
-        console.log("OUT" + localStorage.getItem("scrollTop")); 
+      console.log("OUT itemsMode " + itemsMode + " " + (ara != null));
+      if(localStorage.getItem("scrollTop") == null & itemsMode === mode[1]){
+        console.log("setItem scrollTop : " +  ara);
+        localStorage.setItem("scrollTop", ara);
       }
-      prevItemMode = itemsMode;
-  }},[itemsMode]);
+  }});
 
   return (
     
-    <div className={css.wrapper} ref={listRef} preventScrollReset={true}>
+    <div className={css.wrapper}>
       <PageHeader
         title="Digiseller"
         subTitle="Панель управления Digiseller ботом"
       />
-      
-      <div className={css.content} preventScrollReset={true}>
-        {itemsMode === mode[1] && <List preventScrollReset={true} />}
-        {itemsMode === mode[2] && <PriceHierarchy preventScrollReset={true} />}
+      <Link onClick={() => {
+        console.log("contentRef.scrollTop : " + contentRef.current.scrollTop);
+      }}>Scrolll</Link>
+      <div className={css.content} ref={contentRef} >
+        {itemsMode === mode[1] && <List />}
+        {itemsMode === mode[2] && <PriceHierarchy />}
       </div>
+      <ScrollRestoration
+        getKey={(location, matches) => {
+          // default behavior
+          console.log("ScrollRestoration");
+          console.log(location);
+          return location.key;
+        }}
+      />
     </div>
   );
 };
