c822e1de2fb8d5600952e01b190172afc8267ea2
Логинов Виктор <loginov.viktor@sogaz-med.ru>
2024-06-28 01:15:13 +0500

    Параллельная отправка игр

--- SteamDigiSellerBot.Services/Implementation/GameSessionManager/SendGameGSQ.cs
+++ SteamDigiSellerBot.Services/Implementation/GameSessionManager/SendGameGSQ.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 using System.Linq;
 using SteamDigiSellerBot.Database.Contexts;
 using SteamDigiSellerBot.Database.Entities;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace SteamDigiSellerBot.Services.Implementation
 {
@@ -45,37 +47,55 @@ namespace SteamDigiSellerBot.Services.Implementation
                         //.ListAsync(gs => gs.StatusId == 18).Result;
                         .GetGameSessionForPipline(gs => gs.Stage == Database.Entities.GameSessionStage.SendGame);
 
+
+                    var tasks = new List<Task>();
+                    int delayCounter = 0;
                     foreach (var gs in sess)
                     {
-                        try
+                        tasks.Add(Task.Factory.StartNew(async () =>
                         {
-                            //if (!q.ContainsKey(gs.Id))
-                            //{
-                            //    SendToManager(new Untracked { gsId = gs.Id });
-                            //    continue;
-                            //}
-                            if (new GameSessionStatusEnum[] { GameSessionStatusEnum.IncorrectProfile, GameSessionStatusEnum.BotNotFound }.Contains(gs.StatusId))
+                            try
                             {
-                                SendToManager(new ToFixStage { gsId = gs.Id });
-                                continue;
-                            }
+                                //if (!q.ContainsKey(gs.Id))
+                                //{
+                                //    SendToManager(new Untracked { gsId = gs.Id });
+                                //    continue;
+                                //}
+                                if (new GameSessionStatusEnum[]
+                                        { GameSessionStatusEnum.IncorrectProfile, GameSessionStatusEnum.BotNotFound }
+                                    .Contains(gs.StatusId))
+                                {
+                                    SendToManager(new ToFixStage { gsId = gs.Id });
+                                    return;
+                                }
 
 
-                            var (sendRes, readyState) = await gss.SendGame(gs.Id);
-                            SendToManager(sendRes == SendGameStatus.sended
-                                ? new Sended { gsId = gs.Id, SendStatus = sendRes, ReadyStatus = readyState }
-                                : new SendFailed { gsId = gs.Id, SendStatus = sendRes, ReadyStatus = readyState, ChangeBot = readyState== GameReadyToSendStatus.botSwitch}
+                                var (sendRes, readyState) = await gss.SendGame(gs.Id);
+                                SendToManager(sendRes == SendGameStatus.sended
+                                    ? new Sended { gsId = gs.Id, SendStatus = sendRes, ReadyStatus = readyState }
+                                    : new SendFailed
+                                    {
+                                        gsId = gs.Id, SendStatus = sendRes, ReadyStatus = readyState,
+                                        ChangeBot = readyState == GameReadyToSendStatus.botSwitch
+                                    }
                                 );
 
-                            //!!!!!!!!!!!!!!!!
-                            //РАСКОМЕНТИТЬ, КАК ТОЛЬКО ПОЧИНИТЬСЯ ОТПРАВКА
-                            continue;
-                        }
-                        catch (Exception ex)
-                        {
-                            _logger.LogError(ex, $"SendGameGSQ GS ID {gs.Id}");
-                        }
+                                //!!!!!!!!!!!!!!!!
+                                //РАСКОМЕНТИТЬ, КАК ТОЛЬКО ПОЧИНИТЬСЯ ОТПРАВКА
+                                return;
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogError(ex, $"SendGameGSQ GS ID {gs.Id}");
+                            }
+                        }));
+                        delayCounter++;
+                        if (delayCounter % 10 == 0)
+                            await Task.Delay(TimeSpan.FromMinutes(1));
                     }
+
+                    await Task.Delay(1000);
+                    await Task.WhenAll(tasks.ToArray());
                 }
                 catch (Exception ex)
                 {
