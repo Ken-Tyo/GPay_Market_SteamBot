96b7c92015eee89b32afc5310cdfd952e335aceb
unknown <--global>
2024-07-14 21:02:18 +0300

    feat: ссылка на телеграм в футере

---
+++ SteamDigiSellerBot/wwwroot/Source/components/footer/index.js
@@ -0,0 +1,43 @@
+﻿import { useTranslation } from 'react-i18next';
+import { useCookies } from 'react-cookie';
+import useGeoLocation from 'react-ipgeolocation';
+
+import TextSwitch from '../home/textSwitch';
+
+import telegramIcon from '../../icons/telegram.svg';
+import css from './styles.scss';
+
+const Footer = () => {
+  const { i18n } = useTranslation();
+  const [cookies, setCookie] = useCookies();
+  const location = useGeoLocation();
+
+  if (!cookies.ln) {
+    const lang = location.country === 'RU' ? 'ru' : 'en';
+    setCookie('ln', lang, { path: '/', maxAge: 365 * 24 * 60 * 60 });
+    i18n.changeLanguage(lang);
+  } else {
+    if (i18n.language !== cookies.ln) i18n.changeLanguage(cookies.ln);
+  }
+
+  let currLang = i18n.language;
+
+  return (
+    <div className={css.footer}>
+      <a href="https://t.me/GPay_Market" target="_blank" className={css.telegram}>
+        <img src={telegramIcon} alt="telegram"/>
+      </a>
+      <TextSwitch
+        defaultValue={currLang === 'en'}
+        options={['Ru', 'Eng']}
+        onChange={(val) => {
+          const lan = val ? 'en' : 'ru';
+          i18n.changeLanguage(lan);
+          setCookie('ln', i18n.language);
+        }}
+      />
+    </div>
+  );
+}
+
+export default Footer;
--- SteamDigiSellerBot/wwwroot/Source/containers/home/app.js
+++ SteamDigiSellerBot/wwwroot/Source/containers/home/app.js
@@ -1,40 +1,23 @@
-﻿import React, { useEffect, Suspense } from 'react';
-import LastOrders from '../../components/home/lastOrders';
-import OrderState from '../../components/home/orderState';
-import {
+﻿import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-  Outlet,
-  useSearchParams,
 } from 'react-router-dom';
-import { state, apiFetchLastOrders, checkCodeWithParams, init } from './state';
+
+import LastOrders from '../../components/home/lastOrders';
+import OrderState from '../../components/home/orderState';
+import Footer from '../../components/footer';
+
+import { init } from './state';
+
 import css from './styles.scss';
 import './i18n';
-import TextSwitch from '../../components/home/textSwitch';
-import { useTranslation } from 'react-i18next';
-import { useCookies } from 'react-cookie';
-import useGeoLocation from 'react-ipgeolocation';
 
 const Layout = () => {
-  const { t, i18n } = useTranslation();
-  const [cookies, setCookie] = useCookies();
-  const location = useGeoLocation();
-
-  if (!cookies.ln) {
-    let lang = location.country === 'RU' ? 'ru' : 'en';
-    setCookie('ln', lang, { path: '/', maxAge: 365 * 24 * 60 * 60 });
-    i18n.changeLanguage(lang);
-  } else {
-    if (i18n.language !== cookies.ln) i18n.changeLanguage(cookies.ln);
-  }
-
-  let currLang = i18n.language;
-
   return (
     <div className={css.wrapper}>
-      <div className={css.bg}></div>
+      <div className={css.bg}/>
       <div className={css.wrapperBackground}>
         <div style={{ flex: '1 1 auto' }}>
           <LastOrders />
@@ -42,17 +25,7 @@ const Layout = () => {
         <div style={{ flex: '1 1 auto' }}>
           <OrderState />
         </div>
-        <div className={css.footer}>
-          <TextSwitch
-            defaultValue={currLang === 'en'}
-            options={['Ru', 'Eng']}
-            onChange={(val) => {
-              let len = val ? 'en' : 'ru';
-              i18n.changeLanguage(len);
-              setCookie('ln', i18n.language);
-            }}
-          />
-        </div>
+        <Footer/>
       </div>
     </div>
   );
