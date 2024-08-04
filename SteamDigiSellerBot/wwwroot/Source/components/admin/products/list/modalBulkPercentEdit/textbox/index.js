7861283ede8c07136f5dad217775a23b8f9f6113
porlov <porlov@alfabank.ru>
2024-08-04 23:57:15 +0300

    Привел в соответствие макету

--- SteamDigiSellerBot/wwwroot/Source/components/admin/products/list/modalBulkPercentEdit/textbox/index.js
+++ SteamDigiSellerBot/wwwroot/Source/components/admin/products/list/modalBulkPercentEdit/textbox/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import css from './styles.scss';
 
-const TextBox = ({ hint, onChange, defaultValue, cymbol }) => {
+const TextBox = ({ hint, onChange, defaultValue, cymbol, width, fontSize, paddingLeft }) => {
   const onChangeText = (event) => {
     let val = event.target.value;
     if (onChange) onChange(val);
   };
 
   return (
-    <div className={css.wrapper} onChange={onChangeText}>
+    <div className={css.wrapper} onChange={onChangeText} style={{
+      width: width ?? '112px'      
+    }}>
       <div className={css.inputControl}>
         <div className={css.inputArea}>
-          <input type={'text'} defaultValue={defaultValue} />
+          <input type={'text'} defaultValue={defaultValue} style={{
+            fontSize: fontSize ?? '14px',
+            paddingLeft: paddingLeft ?? '15px'
+          }} />
           {cymbol && <div className={css.cymbol}>{cymbol}</div>}
         </div>
       </div>
