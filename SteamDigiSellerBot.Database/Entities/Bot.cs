55534834ca5ae2ce3a91c7c6210a4c2de4d7b978
Логинов Виктор <loginov.viktor@sogaz-med.ru>
2024-10-30 11:30:02 +0500

    Возвращение после шифрования ботов

--- SteamDigiSellerBot.Database/Entities/Bot.cs
+++ SteamDigiSellerBot.Database/Entities/Bot.cs
@@ -93,7 +93,7 @@ namespace SteamDigiSellerBot.Database.Entities
         {
             get
             {
-                return new CookieDictionary(!string.IsNullOrEmpty(SteamCookiesStrC) ? SteamCookiesStrC : CryptographyUtilityService.Decrypt(SteamCookiesStr));
+                return new CookieDictionary(string.IsNullOrEmpty(SteamCookiesStrC) ? SteamCookiesStr : CryptographyUtilityService.Decrypt(SteamCookiesStr));
             }
             set
             {
