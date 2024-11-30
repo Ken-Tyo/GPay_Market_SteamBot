26e013354c08ae624b034a15f037481dedb64afa
alexander2202 <bigzikan@mail.ru>
2024-11-30 18:42:30 +0500

    20 Добавлена таблица Customers для пользователей-покупателей

--- SteamDigiSellerBot.Database/Migrations/20241130133640_AddCustomers.Designer.cs
+++ SteamDigiSellerBot.Database/Migrations/20241130133640_AddCustomers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SteamDigiSellerBot.Database.Contexts;
@@ -12,9 +13,10 @@ using SteamKit2.Internal;
 namespace SteamDigiSellerBot.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241130133640_AddCustomers")]
+    partial class AddCustomers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
