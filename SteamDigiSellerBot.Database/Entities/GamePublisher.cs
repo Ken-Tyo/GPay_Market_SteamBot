657a7adac1d1c40fdb3bdaf770c2b53a50587fc4
KovalNikitok <nikitakovalmen2@gmail.com>
2024-12-11 19:38:31 +0700

    Добавлена модель данных для получения игровых издателей

---
+++ SteamDigiSellerBot.Database/Entities/GamePublisher.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseRepository.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SteamDigiSellerBot.Database.Entities
+{
+    [Table("GamePublishersView")]
+    public class GamePublisher : BaseEntity
+    {
+        public uint GamePublisherId { get; set; }
+        public int GameId { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("GameId")]
+        public virtual Game Game { get; set; }
+    }
+}
